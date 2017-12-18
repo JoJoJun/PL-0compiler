22592a79a341228a5d79657046d49f7d099cfa4d
JoJoJun <mojinxinzhu@163.com>
2017-12-18 16:13:22 +0800

    Add files via upload
    Tokens-only C#

---
+++ Tokens/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Tokens
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SourceText = new System.Windows.Forms.TextBox();
+            this.Results = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(414, 182);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "读取文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // SourceText
+            // 
+            this.SourceText.AcceptsReturn = true;
+            this.SourceText.AcceptsTab = true;
+            this.SourceText.AllowDrop = true;
+            this.SourceText.Font = new System.Drawing.Font("Consolas", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SourceText.Location = new System.Drawing.Point(92, 32);
+            this.SourceText.Multiline = true;
+            this.SourceText.Name = "SourceText";
+            this.SourceText.Size = new System.Drawing.Size(533, 135);
+            this.SourceText.TabIndex = 1;
+            // 
+            // Results
+            // 
+            this.Results.AcceptsReturn = true;
+            this.Results.AcceptsTab = true;
+            this.Results.AllowDrop = true;
+            this.Results.Font = new System.Drawing.Font("Consolas", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Results.Location = new System.Drawing.Point(99, 221);
+            this.Results.Multiline = true;
+            this.Results.Name = "Results";
+            this.Results.Size = new System.Drawing.Size(526, 128);
+            this.Results.TabIndex = 2;
+            this.Results.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(550, 182);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "编译";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("YouYuan", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(92, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 14);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "源程序";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("YouYuan", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(99, 192);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 14);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "结果";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 384);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Results);
+            this.Controls.Add(this.SourceText);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "简单词法分析程序";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox SourceText;
+        private System.Windows.Forms.TextBox Results;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Tokens/Form1.cs
@@ -0,0 +1,442 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace Tokens
+{
+    public partial class Form1 : Form
+    {
+        string filePath="";
+         string text = "";
+         int len;
+         char CHAR;//当前读到的字符
+         string token;//当前处理的单词
+         int num;
+         float f;
+         bool IsEnd = false;
+         string errorInf = "";
+         int i = -1;
+         enum classify
+         {
+             KEYWORDSY = 0,/*keyword*/
+             CALSY = 3,/*分解符*/
+             IDSY = 1,/*identifier*/
+             INTSY = 2,/*整数*/
+             FLOATSY = 4,/*浮点数*/
+             COMMENTSY = 5/*注释*/,
+             SINGLESY=7,/*单字符运算符*/
+             DOUBLE=8,/*双字符运算符*/
+         }
+         classify symbol;
+         string outt = "";
+         string[] Kind = { "关键字", "标识符", "整数", "分界符", "浮点数", "注释", "" ,"单运算符","双运算符"};
+         string[] res = { "begin", "end", "if", "then", "else", "const", "while", "var", "procedure", "Ood", "call", "write", "read","repeat","until" };
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.ControlBox = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new System.Windows.Forms.OpenFileDialog();//一个打开文件的对话框    
+            openFileDialog1.Filter = "文本文件(*.txt)|*.txt";//设置允许打开的扩展名    
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)//判断是否选择了文件      
+            {
+                filePath = openFileDialog1.FileName;//打开文件的路径  
+                SourceText.Text = "";//清空textBox1  
+                StreamReader streamReader = new StreamReader(filePath, Encoding.Default);//记录用户选择的文件路径  
+                while (!streamReader.EndOfStream)
+                {//如果这个还没有读到文件尾  
+                    string line = streamReader.ReadLine();//就一行一行地读  
+                    SourceText.Text += line + "\r\n";
+                }
+                text = SourceText.Text;
+                len = text.Length;
+                streamReader.Close();//一定要关闭这个流，不然会和下面保存文件的流冲突  
+               // SourceText.ScrollBars = ScrollBars.Vertical;
+                SourceText.ScrollBars = ScrollBars.Both;
+            }
+            else
+            {
+                MessageBox.Show("请打开txt文件");
+            }  
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (text != "")
+            {
+                Results.ScrollBars = ScrollBars.Vertical;
+                Results.ScrollBars = ScrollBars.Both;
+                Results.Text = "";
+                i = -1;
+                Results.Text = "单词\t\t类别\t\t值\r\n";
+                Console.WriteLine(Results.Text);
+                IsEnd = false;
+                while (IsEnd==false)
+                {
+                    errorInf = "";
+                    int t = getsym();
+                    if (IsEnd == false)
+                    {
+                        Results.Text = Results.Text + token;
+                        if (token == "procedure") Results.Text += "\t";
+                        else Results.Text += "\t\t";
+                       Results.Text +=Kind[(int)symbol];
+                        if (Kind[(int)symbol] == "单运算符" || Kind[(int)symbol] == "双运算符") Results.Text += "\t";
+                        else Results.Text += "\t\t";
+                        Results.Text+=outt + errorInf + "\r\n";
+                    }
+                    this.Refresh();
+                   Console.Write(Results.Text);
+                }
+                MessageBox.Show("编译结束");
+                
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+        void clearToken()
+        {
+            token = "";
+            outt = "";
+        }
+        void catToken()
+        {
+            token = token + CHAR;
+        }
+        int reserver()
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                if (res[i] == token)
+                {
+                    return i;
+                    //break;
+                }
+            }
+            return -1;
+        }
+
+        bool isColon()
+        {
+            if (CHAR == ':')
+                return true;
+            else return false;
+        }
+        bool isEqu()
+        {
+            if (CHAR == '=')
+                return true;
+            else return false;
+        }
+        bool isPlus()
+        {
+            if (CHAR == '+')
+                return true;
+            else return false;
+        }
+        bool isMinus()
+        {
+            if (CHAR == '-')
+                return true;
+            else return false;
+        }
+        bool isStar()
+        {
+            if (CHAR == '*')
+                return true;
+            else return false;
+        }
+        bool isLpar()
+        {
+            if (CHAR == '(')
+                return true;
+            else return false;
+        }
+        bool isRpar()
+        {
+            if (CHAR == ')')
+                return true;
+            else return false;
+        }
+        bool isComma()
+        {
+            if (CHAR == ',')
+                return true;
+            else return false;
+        }
+        bool isSemi()
+        {
+            if (CHAR == ';')
+                return true;
+            else return false;
+        }
+        bool isDivi()
+        {
+            if (CHAR == '/')
+                return true;
+            else return false;
+        }
+        bool isSmall()
+        {
+            if (CHAR == '<')
+                return true;
+            else return false;
+        }
+        bool isBig()
+        {
+            if (CHAR == '>')
+                return true;
+            else return false;
+        }
+        bool isDot()
+        {
+            if (CHAR == '.')
+                return true;
+            else return false;
+        }
+        void error()
+        {
+            Console.WriteLine("This word is error!\n");
+            MessageBox.Show(token +" here is wrong!");
+            errorInf = "This word is error!";
+            symbol = (classify)6;
+        }
+        string returnError()
+        {
+            return errorInf;
+        }
+        void retract()
+        {
+            if (i > 0)
+            {
+                i--;
+            }
+            else
+            {
+                i = 0;
+            }
+            CHAR = text[i];
+        }
+        void getChar()
+        {
+            if (i < len - 1)
+            {
+                i++;
+                CHAR = text[i];
+            }
+            else { CHAR = '\0'; IsEnd = true; }
+
+        }
+
+        string transNum()
+        {
+            int numVal = Int32.Parse(token);
+            string str = Convert.ToString(numVal, 2);
+            return str;
+        }
+        string transFloat()
+        {
+            string ans = "";
+            return ans;
+        }
+
+
+
+
+        int getsym()
+        {
+            if (IsEnd == false)
+            {
+                clearToken();
+                getChar();
+                //cout << "first get char  " << CHAR << endl;
+                while (CHAR=='\r' || Char.IsWhiteSpace(CHAR) || CHAR == '\t'||CHAR=='\n'||CHAR==' ') { getChar(); }
+                //字母
+                if (Char.IsLetter(CHAR))
+                {
+                    catToken();
+                    getChar();
+                    while (Char.IsLetter(CHAR) || Char.IsDigit(CHAR))
+                    {
+                        catToken(); getChar();
+                    }
+                    retract();
+                    int resultValue = reserver();
+                    if (resultValue == -1) { symbol = classify.IDSY; outt = token; }
+                    else { symbol = classify.KEYWORDSY; outt = token; }
+                }
+                //数字
+                else if (Char.IsDigit(CHAR))
+                {
+                    catToken();
+                    getChar();
+                    while (Char.IsDigit(CHAR))
+                    {
+                        catToken(); getChar();
+                    }
+                    //整数
+                    if (!isDot())
+                    {
+                        retract();
+                        outt = transNum();
+                        symbol = classify.INTSY;
+
+                    }
+                    else
+                    {
+                        catToken();
+                        getChar();
+                        while (Char.IsDigit(CHAR))
+                        {
+                            catToken(); getChar();
+                        }
+                        if (isDot())
+                        {
+                           
+                            catToken();
+                            getChar();
+                            while (Char.IsDigit(CHAR))
+                            {
+                                catToken(); getChar();
+                            }
+                            symbol = (classify)6;
+                            error();
+                             //MessageBox.Show(token +" here is wrong!");
+                        }
+                        else { symbol = classify.FLOATSY; }
+
+                        retract();
+
+                        outt = token;
+                    }
+                }
+                else if (isColon())
+                {
+                    catToken();
+                    getChar();
+                    if (isEqu()) { catToken(); symbol = classify.DOUBLE; outt = ":="; }
+                    else { retract(); symbol = classify.CALSY; outt = "："; }
+                }
+                else if (isEqu()) { catToken(); symbol = classify.SINGLESY; outt = "="; }
+                else if (isPlus()) { catToken(); symbol = classify.SINGLESY; outt = "+"; }
+                else if (isMinus()) { catToken(); symbol = classify.SINGLESY; outt = "-"; }
+                else if (isStar()) { catToken(); symbol = classify.SINGLESY; outt = "*"; }
+                else if (isLpar()) { catToken(); symbol = classify.CALSY; outt = "（"; }
+                else if (isRpar()) { catToken(); symbol = classify.CALSY; outt = "）"; }
+                else if (isComma()) { catToken(); symbol = classify.CALSY; outt = "，"; }
+                else if (isSemi()) { catToken(); symbol = classify.CALSY; outt = ";"; }
+                else if (isSmall())
+                {
+                    catToken();
+                    getChar();
+                    if (isEqu()) { catToken(); symbol = classify.DOUBLE; outt = "<="; }
+                    else
+                    {
+                        retract(); symbol = classify.SINGLESY; outt = "<";
+                    }
+                }
+                else if (isBig())
+                {
+                    catToken();
+                    getChar();
+                    if (isEqu()) { catToken(); symbol = classify.DOUBLE; outt = ">="; }
+                    else
+                    {
+                        retract(); symbol = classify.SINGLESY; outt = ">";
+                    }
+                }
+                else if(isDot())
+                {
+                    catToken();
+                    symbol = classify.CALSY;outt=".";
+                }
+                else if (isDivi())
+                {
+
+                    getChar();
+                    if (isStar())
+                    {
+                        do
+                        {
+                            do { getChar(); } while (!isStar());
+                            do
+                            {
+                                getChar();
+                                if (isDivi())
+                                {
+                                    symbol = classify.COMMENTSY;
+                                    outt = ""; return 0;
+                                }
+                            } while (isStar());
+                        } while (!isStar());
+                        symbol = classify.COMMENTSY;
+                        outt = "";
+                    }
+                    else
+                    {
+                        retract();
+                        catToken();
+                        symbol = classify.SINGLESY; outt = "/";
+                    }
+                }
+                else if (IsEnd == true) return 0;
+                else { catToken(); error(); }
+                return 0;
+            }
+            return -1;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult result;
+            result = MessageBox.Show("确定退出吗？", "退出", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (result == DialogResult.OK)
+            {
+                Application.ExitThread();
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+
+        /*private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        */
+
+
+
+
+
+
+
+
+    }
+}
